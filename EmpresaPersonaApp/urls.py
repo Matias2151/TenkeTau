70a3cd4a72a0a6cfeeedcf723e04187dc88b0238
Matias Allendes <mallendes2005@gmail.com>
2025-11-18 01:22:56 -0300

    Delete EmpresaPersonaApp directory

--- EmpresaPersonaApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("empresa_clientes/", views.empresa_clientes, name="empresa_clientes"),
-    path("editar/<int:pk>/", views.editar_persona, name="editar_persona"),
-    path("eliminar/<int:pk>/", views.eliminar_persona, name="eliminar_persona"),
-    path("obtener_personas_json/", views.obtener_personas_json, name="obtener_personas_json"),
-]
