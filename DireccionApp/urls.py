3e2bc90ef6bdb838073d453eeab85b3b18c3faee
Matias Allendes <mallendes2005@gmail.com>
2025-11-28 11:21:41 -0300

    Delete DireccionApp directory

--- DireccionApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ciudades/<int:regi_id>/', views.ciudades_por_region, name='ciudades_por_region'),
-    path('comunas/<int:ciuda_id>/', views.comunas_por_ciudad, name='comunas_por_ciudad'),
-]
