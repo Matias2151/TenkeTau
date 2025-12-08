74745eb5538be48ca8f9a1bc7b34b555b8647fd2
Matias Allendes <mallendes2005@gmail.com>
2025-12-08 20:37:04 -0300

    Create default admin user with hashed password

--- UsuariosApp/migrations/0002_useradmin.py
+++ UsuariosApp/migrations/0002_useradmin.py
@@ -1,17 +1,22 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
 
 def crear_admin_por_defecto(apps, schema_editor):
     UsuarioSistema = apps.get_model("UsuariosApp", "UsuarioSistema")
 
     if not UsuarioSistema.objects.filter(username="admin").exists():
-        usuario = UsuarioSistema(
+        UsuarioSistema.objects.create(
             username="admin",
             email="admin@teknetau.cl",
             role="admin",
             is_active=True,
+            # 👇 encriptamos la contraseña igual que set_password
+            password=make_password("admin123"),
+            # 👇 y dejamos la visible en texto plano
+            password_visible="admin123",
         )
-        usuario.set_password("admin123")  # Se guarda encriptada
-        usuario.save()
+
 
 def eliminar_admin(apps, schema_editor):
     UsuarioSistema = apps.get_model("UsuariosApp", "UsuarioSistema")
